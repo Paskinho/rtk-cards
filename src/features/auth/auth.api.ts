ed7ce4235659684c25baea2da5935b66d0b5a2a8
Paskinho <v.paskanov@gmail.com>
2023-05-11 14:00:53 +0300

    1 lesson. Some typization and refactoring with arg

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -1,11 +1,13 @@
 import {instance} from "common/api/common.api";
 
 export const authApi = {
-    register: () => {
-        const payload: any = {
-            email: "nya-admin@nya.nya",
-            password: "1qazxcvBG"
-        }
-return instance.post('auth/register', payload )
+    register: (arg: ArgRegisterType) => {
+return instance.post('auth/register', arg )
     }
+}
+
+
+export type ArgRegisterType = {
+    email: string,
+    password: string
 }
--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -1,10 +1,10 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {authApi} from "features/auth/auth.api";
+import {ArgRegisterType, authApi} from "features/auth/auth.api";
 
 
-const register = createAsyncThunk('auth/register', (arg: any, thunkAPI)=> {
+const register = createAsyncThunk('auth/register', (arg: ArgRegisterType, thunkAPI)=> {
     const {dispatch, getState, rejectWithValue} = thunkAPI
-    authApi.register().then((res)=> {
+    authApi.register(arg).then((res)=> {
 
     })
 })
