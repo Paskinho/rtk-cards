8959df3f282268409b0a494436ab066dd075cc17
Paskinho <v.paskanov@gmail.com>
2023-05-11 21:05:13 +0300

    1 lesson. Add typization UserType

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -11,3 +11,20 @@ export type ArgRegisterType = {
     email: string,
     password: string
 }
+
+export type RegistreResponsetype = {
+    addedUser: UserType
+}
+
+export type UserType = {
+    _id: string;
+    email: string;
+    rememberMe: string;
+    isAdmin: boolean;
+    name: string;
+    verified: boolean;
+    publicCardPacksCount: number;
+    created: string;
+    updated: string;
+    __v: number;
+}
