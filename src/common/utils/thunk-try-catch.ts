a07c970d9d38129611399abbc64d8f2308d71a77
Paskinho <v.paskanov@gmail.com>
2023-05-30 19:45:15 +0300

    2 lesson. Refactoring thunk-try-catch.ts

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -1,14 +1,21 @@
 import {BaseThunkAPI} from "@reduxjs/toolkit/dist/createAsyncThunk";
 import {AppDispatch, RootState} from "app/store";
 import {appActions} from "app/app.slice";
+import {AxiosError, isAxiosError} from "axios";
 
-export const thunkTryCatch = async (thunkAPI: BaseThunkAPI<RootState, any, AppDispatch, unknown>, logic: Function) => {
+export const thunkTryCatch = async (thunkAPI: BaseThunkAPI<RootState, any, AppDispatch, unknown>,
+                                    logic: Function) => {
     const { dispatch, rejectWithValue } = thunkAPI;
     try {
         return await logic();
-    } catch (e: any) {
-        const error = e.response ? e.response.data.error : e.message;
-        dispatch(appActions.setError({ error }));
+    } catch (e) {
+        const err = e as Error | AxiosError<{ error: string }>;
+        if (isAxiosError(err)) {
+            const error = err.response ? err.response.data.error : err.message;
+            dispatch(appActions.setError({ error }));
+        } else {
+            dispatch(appActions.setError({ error: `Native error ${err.message}` }));
+        }
         return rejectWithValue(null);
     }
 };
