a0658cb501f63c60d14f4b4f3ae03ec943f89b62
Paskinho <v.paskanov@gmail.com>
2023-06-13 19:38:27 +0300

    HW after 1.  Some changes in RecoveryPassword.tsx

--- src/features/auth/RecoveryPassword/RecoveryPassword.tsx
+++ src/features/auth/RecoveryPassword/RecoveryPassword.tsx
@@ -5,5 +5,7 @@ export const RecoveryPassword = () => {
     return <div className={s.container}>
         <h1>Forgot you Password?</h1>
         <input className={s.input} placeholder={'Email'} type={"email"}/>
+        <button className={s.sign}>Send Instructions</button>
+        <div className={s.registration}>Try logging in</div>
     </div>
 };
