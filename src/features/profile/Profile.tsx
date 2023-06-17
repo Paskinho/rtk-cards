c7518f28171b39b3951d340bd83e38c45b79cdc0
Paskinho <v.paskanov@gmail.com>
2023-06-17 18:46:14 +0300

    HW after 1.  Some refactoring Profile.tsx

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,14 +1,16 @@
 import { useAppDispatch } from "common/components/hooks";
 import {authThunks} from "features/auth/auth.slice";
-
+import sCommon from'../../common/styles/styles.module.css'
+import ProfilePhoto from '../../assets/images/Profile.jpg'
 
 export const Profile = () => {
     const dispatch = useAppDispatch()
 
     // dispatch(authThunks.profile)
     return (
-        <div>
-            Profile
+        <div className={sCommon.container}>
+            <h1>Personal Information</h1>
+            <img src={ProfilePhoto}/>
         </div>
     )
 }
