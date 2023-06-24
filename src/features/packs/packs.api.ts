bb58420afabafaeed6526dadbab60bc6fa9ecdb1
Paskinho <v.paskanov@gmail.com>
2023-06-24 12:31:05 +0300

    HW after 2. Create Packs.tsx and api

--- src/features/packs/packs.api.ts
+++ src/features/packs/packs.api.ts
@@ -1,5 +1,26 @@
+import {instance} from "../../common/api/common.api";
 
 
 export const packsApi = {
+    getPacks: (arg: GetPacksType) => {
+        return instance.get<GetPacksType>('/cards/pack') // Уточнить по arg
+    }
+}
 
+export type GetPacksType = {
+    cardPacks: CardsPackType
+    cardPacksTotalCount: number
+    maxCardsCount: number
+    minCardsCount: number
+    page: number
+    pageCount: number
+}
+
+type CardsPackType = {
+    _id: string
+    user_id: string
+    name: string
+    cardsCount: number
+    created: string
+    updated: string
 }
--- src/features/packs/packs.slice.ts
+++ src/features/packs/packs.slice.ts
@@ -1,5 +1,8 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {ProfileType, SetNewPasswordType} from "features/auth/auth.api";
+import {authApi, ProfileType, SetNewPasswordType} from "features/auth/auth.api";
+import {createAppAsyncThunk} from "../../common/utils/createAppAsyncThunk";
+import {packsApi} from "./packs.api";
+import {thunkTryCatch} from "../../common/utils/thunkTryCatch";
 
 const slice = createSlice({
     name: "packs",
@@ -7,6 +10,13 @@ const slice = createSlice({
     },
     reducers: {},
 });
+
+const getPacks = createAppAsyncThunk ('packs/getPacks', async (arg , thunkAPI)=> {
+    return thunkTryCatch(thunkAPI, async () => {
+        const res = await packsApi.getPacks(arg);
+    });
+
+})
 
 
 export const packsReducer = slice.reducer
